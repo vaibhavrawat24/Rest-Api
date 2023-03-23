2ee260fc74e1d34ad21e17442c21d7a7f6045376
Vaibhav <rawatvaibhav42@gmail.com>
2023-03-23 22:24:32 +0530

    added file

--- fit-api.py
+++ fit-api.py
@@ -6,22 +6,22 @@
 # result = r.json()
 # print(result)
 
-# import requests
-
-# url = "https://v1.nocodeapi.com/vaibhavr42/fit/McsSKrXlnsaKjLFm/aggregatesDatasets?dataTypeName=steps_count&timePeriod=today&durationTime=hourly"
-# params = {}
-# r = requests.get(url = url, params = params)
-# result = r.json()
-
-# # Extract the step count value from the JSON response
-# steps = result['steps_count'][0]['value']
-
-# print(steps) # Output: 3272
 import requests
 
-url = "https://v1.nocodeapi.com/vaibhavr42/fit/McsSKrXlnsaKjLFm/aggregatesDatasets?dataTypeName=weight&timePeriod=today"
+url = "https://v1.nocodeapi.com/vaibhavr42/fit/McsSKrXlnsaKjLFm/aggregatesDatasets?dataTypeName=steps_count&timePeriod=today&durationTime=hourly"
 params = {}
 r = requests.get(url = url, params = params)
 result = r.json()
-weightt=result['weight'][0]['value']
-print(weightt)
+
+# Extract the step count value from the JSON response
+steps = result['steps_count'][0]['value']
+
+print(steps) # Output: 3272
+# import requests
+
+# url = "https://v1.nocodeapi.com/vaibhavr42/fit/McsSKrXlnsaKjLFm/aggregatesDatasets?dataTypeName=weight&timePeriod=today"
+# params = {}
+# r = requests.get(url = url, params = params)
+# result = r.json()
+# weightt=result['weight'][0]['value']
+# print(weightt)
